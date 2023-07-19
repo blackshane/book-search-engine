dd5dc9d6fb3e36cee9a2e03174ac9098593eec43
blackshane <122633142+blackshane@users.noreply.github.com>
2023-07-18 19:54:12 -0500

    resolver mutation for adding user

--- server/models/User.js
+++ server/models/User.js
@@ -21,14 +21,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
+    //  savedBooks to be an array of data that adheres to the bookSchema
     savedBooks: [{
       type: Schema.Types.ObjectId,
       ref: 'Book',
       book: bookSchema
     }],
   },
-  // set this to use virtual below
+  // Set virtual
   {
     toJSON: {
       virtuals: true,
@@ -36,7 +36,7 @@ const userSchema = new Schema(
   }
 );
 
-// hash user password
+// Hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
@@ -46,12 +46,12 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-// custom method to compare and validate password for logging in
+// Custom method to compare and validate login password
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+// User query creates `bookCount` with the number of saved books we have
 userSchema.virtual('bookCount').get(function () {
   return this.savedBooks.length;
 });
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,19 @@ Query: {
     users: async () => {
         return await User.find({})
     }
+},
+
+Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+    },
+},
+
+
+
+
 }
-};
 
 module.exports = resolvers;

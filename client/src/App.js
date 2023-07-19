364543e3b94a6722e5d6a6825c61723e6e8b85f4
blackshane <122633142+blackshane@users.noreply.github.com>
2023-07-18 19:28:33 -0500

    auth update

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,16 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Auth from './utils/auth';
+
+const token = Auth.getToken() || null;
 
 const client = new ApolloClient({
   url: '/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${token}`,
+  }
 });
 
 function App() {
@@ -17,8 +23,8 @@ function App() {
         <>
           <Navbar />
           <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
+            <Route exact path='/' element={ <SearchBooks />} />
+            <Route exact path='/saved' component={ <SavedBooks />} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
         </>

6e28b7066abbcd854593fc55c711c2bea4f07960
blackshane <122633142+blackshane@users.noreply.github.com>
2023-07-19 21:01:16 -0500

    server update for Heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: authMiddleware 
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -25,8 +25,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
@@ -38,7 +36,5 @@ const startApolloServer = async () => {
     })
   })
   };
-  
-// Call the async function to start the server
-  startApolloServer();
 
+  startApolloServer();
